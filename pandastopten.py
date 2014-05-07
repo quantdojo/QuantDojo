90a10cd4dc7c18511fa5a0b7eac47544570ac1f7
QuantDojo <QuantDojo@gmail.com>
2014-05-07 06:13:21 -0700

    7
    Continuing Top Ten

--- pandastopten.py
+++ pandastopten.py
@@ -68,7 +68,7 @@ print(df.ix[:,['int_col','float_col']].apply(np.sum))
 
 print(df.applymap(some_fn))
 
-### 6. Vectorized mathematical and string operations ###
+### Vectorized mathematical and string operations ###
 
 df = pd.DataFrame(data={"A":[1,2], "B":[1.2,1.3]})
 
@@ -92,5 +92,40 @@ df["F"] = df.Z.str.upper()
 
 print(df)
 
-input()
+### 6. Groupby ###
 
+### tutorial didnt mention reseting df so reseting here ###
+df = DataFrame({'int_col' : [1,2,6,8,-1], 'float_col' : [0.1, 0.2, 0.2, 10.1, None], 'str_col' : ['a','b',None,'c','a']})
+
+grouped = df['float_col'].groupby(df['str_col'])
+
+print(grouped.mean())
+
+### 7. New Columns ###
+
+df4 = df.copy()
+
+def two_three_strings(x):
+    return x*2, x*3
+
+df4['twice'],df4['thrice'] = zip(*df4['int_col'].map(two_three_strings))
+
+print(df4)
+
+df5 = df.copy()
+
+def sum_two_cols(series):
+    return series['int_col'] + series['float_col']
+
+df5['sum_col'] = df5.apply(sum_two_cols,axis=1)
+
+print(df5)
+
+import math
+
+def int_float_squares(series):
+ return pd.Series({'int_sq' : series['int_col']**2, 'flt_sq' : series['float_col']**2})
+
+print(df.apply(int_float_squares, axis = 1))
+
+input()
